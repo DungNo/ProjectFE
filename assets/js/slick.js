4a9b748616794c22c3205727f63245f2362b3910
DungNo <ducdungcntta@gmail.com>
2021-05-24 12:24:57 +0700

    Add file Frontend

---
+++ assets/js/slick.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  $(".slider-mttain").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+
+  });
+
+  $('.products-main').slick({
+    infinite: true,
+    rows: 2,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          rows: 2
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          rows: 2
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows: 2
+        }
+      }
+    ]
+  });
+});
