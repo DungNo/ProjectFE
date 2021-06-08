68974bef70a3813407872bbc7457a1031ca10e3a
DungNo <ducdungcntta@gmail.com>
2021-06-08 15:56:28 +0700

    Update 08/06

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,20 +1,3 @@
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   x[slideIndex-1].style.display = "block";  
-// }
 $(document).ready(function () {
   $('.slider-main').slick({
     slidesToShow: 1,
@@ -25,6 +8,5 @@ $(document).ready(function () {
     prevArrow: $('.prev1'),
     nextArrow: $('.next1')
   });
-  $('.slider-main').slick('refresh');
-  // $('.slider-main')[0].slick.refresh();
+  // $('.slider-main').slick('refresh');
 });
